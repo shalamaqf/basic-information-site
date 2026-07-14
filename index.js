93d45f3f750b93fafae45b5a5c7733590a5a8314
Shalama <shalamafadillaa@gmail.com>
2026-07-14 11:56:01 +0700

    Set header for the response object

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method);
+
+    res.setHeader('Content-Type', 'text/html');
 });
 
 server.listen(8080, 'localhost', () => {

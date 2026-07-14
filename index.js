fc2f47417b800f20f5fe8f39aa2f8e3fa158c768
Shalama <shalamafadillaa@gmail.com>
2026-07-14 11:58:53 +0700

    Create basic routing

--- index.js
+++ index.js
@@ -5,6 +5,23 @@ const server = http.createServer((req, res) => {
     console.log(req.url, req.method);
 
     res.setHeader('Content-Type', 'text/html');
+
+    // basic routing
+    const path = './views/';
+    switch(req.url) {
+        case '/' :
+            path += 'index.html'
+            break;
+        case '/about':
+            path += 'about.html'
+            break;
+        case '/contact-me':
+            path += 'contact-me.html'
+            break;
+        default:
+            path += '404.html'
+            break;
+    }
 });
 
 server.listen(8080, 'localhost', () => {

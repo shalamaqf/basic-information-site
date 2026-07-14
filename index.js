fe9072e3769cabc63fff3e61e818fe3f6a20c402
Shalama <shalamafadillaa@gmail.com>
2026-07-14 11:53:56 +0700

    Create a server

---
+++ index.js
@@ -0,0 +1,10 @@
+// Create a server
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method);
+});
+
+server.listen(8080, 'localhost', () => {
+    console.log('Listen to port 8080')
+})

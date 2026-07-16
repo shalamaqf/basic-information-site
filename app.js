b945925f8b2b218e783da9834cc6966e347f3cba
Shalama <shalamafadillaa@gmail.com>
2026-07-16 10:20:33 +0700

    Add guard check in listen method

--- app.js
+++ app.js
@@ -4,7 +4,13 @@ const express = require('express');
 const app = express();
 
 const port = 3000;
-app.listen(port);
+app.listen(port, (err) => {
+    if (err) {
+        console.log(err);
+    }
+
+    console.log(`My first Express app - listening on port ${port}!`)
+})
 
 
 // routing

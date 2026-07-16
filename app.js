ccab2b94d1c8795b1c110a87b07f09a5096cb8f8
Shalama <shalamafadillaa@gmail.com>
2026-07-16 10:17:07 +0700

    Add routing for html file

--- app.js
+++ app.js
@@ -5,3 +5,21 @@ const app = express();
 
 const port = 3000;
 app.listen(port);
+
+
+// routing
+app.get('/', (req, res) => {
+    res.sendFile('./views/index.html', { root: __dirname });
+})
+
+app.get('/about', (req, res) => {
+    res.sendFile('./views/about.html', { root: __dirname });
+})
+
+app.get('/contact-me', (req, res) => {
+    res.sendFile('./views/contact-me.html', { root: __dirname });
+})
+
+app.use('/', (req, res) => {
+    res.status(404).sendFile('./views/404.html', { root: __dirname });
+})

60254d72c4bd6511713858267fffb1ba19c94c1c
Shalama <shalamafadillaa@gmail.com>
2026-07-17 10:58:44 +0700

    Add try/catch block in getAuthorById function

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -5,14 +5,19 @@ const db = require('../db');
 async function getAuthorById(req, res) {
     const { authorId } = req.params;
 
-    const author = await db.getAuthorById(Number(authorId));
+    try {
+        const author = await db.getAuthorById(Number(authorId));
 
-    if (!author) {
-        res.status(404).send('Author not found');
-        return;
+        if (!author) {
+            res.status(404).send('Author not found');
+            return;
+        }
+
+        res.send(`Author Name: ${author.name}`)
+    } catch (err){
+        console.error("Error retrieving author:", err);
+        res.status(500).send("Internal Server Error");
     }
-
-    res.send(`Author Name: ${author.name}`)
 };
 
 module.exports = { getAuthorById };

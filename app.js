4bf2ca12ccaf1748765b5fb565810c31ae25024f
Shalama <shalamafadillaa@gmail.com>
2026-07-20 08:01:46 +0700

    Create users object

--- app.js
+++ app.js
@@ -16,8 +16,10 @@ const links = [
     { href: '/about', text: "About"}
 ];
 
+const users = ['Rose', 'Cake', 'Biff'];
+
 app.get('/', (req, res) => {
-    res.render("index", { links: links })
+    res.render("index", { links: links, users: users })
 });
 
 app.use('/authors', authorRouter)
